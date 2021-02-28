231957cdacb3fdb46ac3b2a5ab3b00d8d3a98ff2
anzehocevar <anze.hocevar@gmail.com>
2021-02-28 14:03:25 +0100

    updated.2 #3

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 public class Gravitacija{
 
+    public static void main(String[] args) {
+        System.out.println("OIS je zaon! debatable");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(izracun(sc.nextDouble()));
+        System.out.println(gravity());
+    }
+
     public static double izracun(double v){
         final double C = 6.674 * Math.pow(10,-11);
         final double M = 5.972 * Math.pow(10,24);
         final double r = 6.371 * Math.pow(10, 6);
+        final String uwu = "triggered";
         return (C*M)/Math.pow((r+v),2);
-    }
-    public static void main(String[] args) {
-        System.out.println("OIS je zakon!");
-        Scanner sc = new Scanner(System.in);
-        System.out.println(izracun(sc.nextDouble()));
-        System.out.println(gravity());
     }
 
     public static String gravity(){
