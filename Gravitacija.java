382c5942e5122787f890e8824bdf28cba5020a19
selenci <mk2529@student.uni-lj.si>
2021-02-28 13:32:24 +0100

    Dodan izracun #3

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,4 +1,11 @@
 public class Gravitacija{
+
+    public static double izracun(double v){
+        final double C = 6.674 * Math.pow(10,-11);
+        final double M = 5.972 * Math.pow(10,24);
+        final double r = 6.371 * Math.pow(10, 6);
+        return (C*M)/Math.pow((r+v),2);
+    }
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
     }
